dccb0829fdec0183121c7fe895adbef48a63ffa1
Crowdbotics <team@crowdbotics.com>
2020-10-01 16:23:45 +0000

    Update models

--- backend/click_or_treat_20941/wsgi.py
+++ backend/click_or_treat_20941/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'click_or_treat_20941.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "click_or_treat_20941.settings")
 
 application = get_wsgi_application()

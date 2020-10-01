f6d29e36499fe16a323f0886cf52226f474df60a
Crowdbotics <team@crowdbotics.com>
2020-10-01 16:21:52 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/click_or_treat_20941/MainActivity.java
@@ -0,0 +1,15 @@
+package com.click_or_treat_20941;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "click_or_treat_20941";
+    }
+}

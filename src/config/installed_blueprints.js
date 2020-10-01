2eb97520e2d619cbcfb084aace7b975527fc3e08
Crowdbotics <team@crowdbotics.com>
2020-10-01 16:26:59 +0000

    add package blueprint-react-native-camera-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Camera4120397', human_name: 'Camera 4', access_route: 'Camera4120397', icon: 'camera'},
 { name: 'BlankScreen17120396', human_name: 'Blank Screen 17', access_route: 'BlankScreen17120396'},
 { name: 'Settings120364', human_name: 'Settings', access_route: 'Settings120364'},
 { name: 'Settings120347', human_name: 'Settings', access_route: 'Settings120347'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Camera4120397Navigator from '../features/Camera4120397/navigator';
 import BlankScreen17120396Navigator from '../features/BlankScreen17120396/navigator';
 import Settings120364Navigator from '../features/Settings120364/navigator';
 import Settings120347Navigator from '../features/Settings120347/navigator';
@@ -16,6 +17,7 @@ import Settings120347Navigator from '../features/Settings120347/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Camera4120397: { screen: Camera4120397Navigator },
 BlankScreen17120396: { screen: BlankScreen17120396Navigator },
 Settings120364: { screen: Settings120364Navigator },
 Settings120347: { screen: Settings120347Navigator },
